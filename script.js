758afbccd3b9ae4ce5ef70f08f57f8655988800c
suchithra1478 <suchithrasandula1478@gmail.com>
2025-07-18 16:35:44 +0530

    Added img animations in elements section

--- script.js
+++ script.js
@@ -1,74 +1,100 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
-function firstPageAni(){
-    var tl = gsap.timeline();
+function firstPageAni() {
+  var tl = gsap.timeline();
 
-    tl.from("#nav", {
-        y: '-10',
-        opacity: 0,
-        duration: 2,
-        ease: Expo.easeInOut
-    })
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 2,
+    ease: Expo.easeInOut,
+  })
 
     .to(".boundingelem", {
-        y: 0,
-        ease: Expo.easeInOut,
-        duration: 1.5,
-        delay: -1,
-        satgger: .2
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 1.5,
+      delay: -1,
+      stagger: 0.2,
     })
 
     .from("#herofooter", {
-        y: -10,
-        opacity: 0,
-        duration: 1.5,
-        delay: -1,
-        ease: Expo.easeInOut
-    })
-    
-    
-
-
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
 }
 
 var timeOut;
 
-function circleSkew(){
-    var xscale = 1;
-    var yscale = 1;
-    var xprev = 0;
-    var yprev = 0;
-    window.addEventListener("mousemove", function(dets){
-        clearTimeout(timeOut);
-        
-        xscale = gsap.utils.clamp(.8,1.2, dets.clientX - xprev);
-        yscale = gsap.utils.clamp(.8,1.2, dets.clientY - yprev);
+function circleSkew() {
+  var xscale = 1;
+  var yscale = 1;
+  var xprev = 0;
+  var yprev = 0;
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeOut);
 
-        xprev = dets.clientX;
-        yprev = dets.clientY;
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
 
-        circleMouse(xscale, yscale);
+    xprev = dets.clientX;
+    yprev = dets.clientY;
 
-        timeOut = setTimeout(function(){
-            document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
-        }, 100);
+    circleMouse(xscale, yscale);
 
-    });
+    timeOut = setTimeout(function () {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
 }
 
 circleSkew();
 
-function circleMouse(xscale, yscale){
-    window.addEventListener("mousemove",function(dets){
-        // this.document.querySelector("#minicircle").style.left = dets.x + "px";
-        // this.document.querySelector("#minicircle").style.top = dets.y + "px";
+function circleMouse(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    // this.document.querySelector("#minicircle").style.left = dets.x + "px";
+    // this.document.querySelector("#minicircle").style.top = dets.y + "px";
 
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
-    })
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
 }
 
 circleMouse();
 firstPageAni();
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+    var rotate = 0;
+    var diffrot = 0;
+    elem.addEventListener("mousemove", function (dets) {
+        var diff = dets.clientY - elem.getBoundingClientRect().top;        
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+
+        gsap.to(elem.querySelector("img"),{
+            opacity: 1,
+            ease: Power3,
+            top: diff,
+            left: dets.clientX,
+            rotate: gsap.utils.clamp(-20, 20, diffrot * 0.8)
+        });
+    });
+
+    elem.addEventListener("mouseleave", function(dets){
+        gsap.to(elem.querySelector("img"),{
+            opacity: 0,
+            ease: Power3,
+            duration: 0.5,
+        });
+    });
+ 
+});
